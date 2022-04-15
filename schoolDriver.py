1a99b041d98daf4edb29219b959e7b7cf61c9234
Médérick Charles Fernand Grivel <grivel.mederick@gmail.com>
2022-04-15 19:31:45 -0400

    updating files

--- schoolDriver.py
+++ schoolDriver.py
@@ -39,4 +39,55 @@ class School:
     def class_rankings():
         """prints the 5 highest ranked students in the class, based on gpa, returns sorted list of the students based on gpa,descending"""
         
+class Course:
+    
+    def __init__(self, courseName,sections,prereqs):
+        self.courseName=courseName
+        self.sections= sections
+        self.prereqs = prereqs
+        
+class Section:
+    
+    def __init__(self,instructor,time):
+        self.instructor= instructor
+        self.time = time
+        
+class Person:
+    
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+        
+class Student(Person):
+    
+    def __init__(self,year,schedule):
+       super.__init__(self)
+       self.year=year
+        
+    def add_class():
+        """adds a class to a students schedule if their schedule allows for it AND they meet prerequisites"""
+        pass
+    def drop_class():
+        """drops a class, updates the student schedule as needed"""
+        pass
+        
+            
+           
+class Instructor(Person):
+    #courses - list of courses they teach
+    
+    def __init__(self,courses):
+        super.__init__(self)
+        self.courses=courses
+        
+    def give_grade():
+        """
+        Method takes a given student, and assigns them a final grade for a course
+        
+        Side Effects: impacts the students gpa calculation
+        """
+        pass
+               
+
+        
         
