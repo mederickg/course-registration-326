164938d8a3a58dc03310c72561740d651d282b67
Adam Isaacs <adam.isaacs99@gmail.com>
2022-04-29 12:49:02 -0400

    created new outline

---
+++ registration.py
@@ -0,0 +1,31 @@
+class Register:
+    def __init__():
+        pass
+    
+    def add(): 
+        pass
+    
+    def drop():
+        pass 
+    
+    def verify_credits():
+        pass
+    
+    def verify_time():
+        pass 
+    
+    def print_schedule():
+        pass 
+    
+    def can_walk():
+        pass 
+    
+class course_db():
+    def __init__():
+        pass 
+    
+    def create_course(): 
+        pass 
+    
+    def create_section():
+        pass 
