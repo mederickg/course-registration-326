4af164c19819a33a6337f9f7f9f9111c7e687d90
Médérick Charles Fernand Grivel <grivel.mederick@gmail.com>
2022-04-29 13:40:10 -0400

    adding more data

--- registration.py
+++ registration.py
@@ -26,12 +26,12 @@ class Course_db():
     def __init__(self, path):
         self.courses = pd.read_csv(path)
     
-    def create_course(): 
-        pass 
-    
-    def create_section():
-        pass 
-    
+    def add_courses(self, path): 
+        temp = pd.read_csv(path)
+        self.courses = pd.concat([self.courses, temp])
     
 cs1 = Course_db("sample_data.csv")
+
+cs1.add_courses("sample_data_2.csv")
+
 print(cs1.courses)
