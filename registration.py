07314a6ce01073fca2f15de0b3a0c00e7b3df8a1
nramey1 <nramey1@umd.edu>
2022-05-04 18:39:42 -0400

    added dictionary of names pointing to student objects, like how we have students

--- registration.py
+++ registration.py
@@ -1,14 +1,16 @@
 import pandas as pd 
 import re
 from argparse import ArgumentParser
+import sys
 
 class School:
     
     GPAS = {'A': 4.0,'B':3.0,'C':2.0,'D':1.0,}
     
-    def __init__(self,students,courses,faculty):
+    def __init__(self,students,courses,faculty =[]):
 
         self.students= students
+        self.studentsdict = {}
         self.courses= pd.DataFrame(pd.read_csv(courses))
         self.faculty=faculty
     
@@ -30,14 +32,17 @@ class School:
                pattern = r"^(\S+\s\S+), (\d+), (\d+), (\d+)$"
                searched = re.search(pattern, line)
                self.students.append(Student(searched.group(0), searched.group(1), searched.group(2), searched.group(3), self.courses))
+               self.studentsdict[searched.group(0)] = Student(searched.group(0), searched.group(1), searched.group(2), searched.group(3), self.courses)
                pass
            
     def addStudent(self, name, age, year, schedule={}):
         #at some point this function will be able to order the schedule 
+        #adds student to studentdict based on their name
         self.students.append(Student(name, age, year, schedule))
     
-    def print_grades(self, Student= None):
+    def print_grades(self, student= None):
         #looks at student grades in each class and prints it
+        # student will be a string of the student name 
         pass
     
     def student_stats(year):
@@ -101,8 +106,63 @@ class Student():
         pass
     
    
+def parse_args(arglist):
+    
+    parser = ArgumentParser("get filepaths to initiate the school program")
+    parser.add_argument("students", help="filepath to a file containing students enrolled in the school") 
+    parser.add_argument("courses", help = "filepath to a file containing the courses held at the school")
+    args = parser.parse_args(arglist)
+    
+    return args  
+     
 if __name__ == "__main__":
-       pass
+    args = parse_args(sys.argv[1:])
+    
+    umd = School(args.students,args.courses)
+    
+    perspective = input("Hello, are you accessing from a student or school perspective? 1 for student, 2 for school") 
+
+    if(perspective == '1'):
+
+            name = input("Please enter your full name in format fname, lname:")
+            age = input("Please enter your age:")
+            year = input("Please enter your class (graduation year):")
+            credits = input("How many credits are you enrolling with?")
+            user = Student(name,int(age),int(year),int(credits)) 
+            umd.students.append(user)
+            track = (umd.students.index(user))
+            
+            option = input("What would you like to do today? \n 1.add class\n 2.drop class\n3.print transcript\n4.exit")
+
+            while(option != 4):
+                
+                if(option == 1):
+                    pass
+                elif(option ==2):
+                    pass
+                elif(option == 3):
+                    pass
+                else:
+                    option =input("please enter a valid option: \n 1.add class\n 2.drop class\n3.print transcript\n4.exit")
+        
+    else:
+        option = input("What would you like to do today?\n 1.add multiple students\n2.add single student\n3.add course \n4. print student\
+        transcript\n5. Exit")
+        
+        while(option!='5'):
+            
+            if(option == '1'):
+                pass
+            elif(option == '2'):
+                pass
+            elif(option == '3'):
+                pass
+            elif(option == '4'):
+                pass
+            elif(option == '5'):
+                break            
+   
+
 
    
            
